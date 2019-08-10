95b7ee31139428b4ccc9afecb42d63df8a15d5f7
jingjing <timesnowkill@sina.cn>
2019-08-10 15:17:59 +0800

    使用 chai 完成 button 的单元测试

--- src/app.js
+++ src/app.js
@@ -15,3 +15,74 @@ new Vue({
         loading3: false,
     }
 })
+
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+
+const expect = chai.expect
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    }).$mount()
+    let useElement = button.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    }).$mount()
+    let useElement = button.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    }).$mount(div)
+    let svg = button.$el.querySelector('svg')
+    expect(window.getComputedStyle(svg).order).to.eq('1')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    }).$mount(div)
+    let svg = button.$el.querySelector('svg')
+    expect(window.getComputedStyle(svg).order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    }).$mount()
+    let spy = chai.spy(function(){})
+    button.$on('click', spy)
+    button.$el.click()
+    expect(spy).to.have.been.called()
+    button.$destroy()
+}
