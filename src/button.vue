cecee92985a2d78cc46304214d9fa8e73d7ce431
jingjing <timesnowkill@sina.cn>
2019-08-10 11:47:06 +0800

    j-button 添加 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,26 @@
 <template>
-    <button class="j-button">按钮</button>
+    <button class="j-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
-
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value) {
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
 </script>
 
@@ -16,6 +32,10 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--border-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover {
         border-color: var(--border-color-hover);
         }
@@ -24,6 +44,24 @@
         }
         &:focus {
             outline: none;
+        }
+        > .icon {
+            order: 1;
+            margin-right: .1em;
+        }
+        > .content {
+            order: 2;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-left: .1em;
+                margin-right: 0;
+            }
+            > .content {
+                order: 1;
+            }
         }
     }
 </style>
