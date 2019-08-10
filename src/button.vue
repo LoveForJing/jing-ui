114b39496b39f8405abf5ef17ab2622c7a674427
jingjing <timesnowkill@sina.cn>
2019-08-10 19:50:00 +0800

    使用 Karma 做自动化测试，Mocha、chai 做单元测试

--- src/button.vue
+++ src/button.vue
@@ -9,7 +9,11 @@
 </template>
 
 <script>
+    import Icon from './icon'
     export default {
+        components: {
+            'j-icon': Icon
+        },
         props: {
             icon: {},
             loading: {
