c3d47e1162b75165a86fdca19eee28262e58eeb2
jingjing <timesnowkill@sina.cn>
2019-08-13 22:22:18 +0800

    完成 col 的 span 属性

---
+++ src/col.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col" :class="colClass">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'JCol',
+        props: {
+            span: {
+                type: [Number, String]
+            }
+        },
+        computed: {
+            colClass() {
+                const { span } = this
+                return span && `col-${span}`
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .col {
+        height: 50px;
+        background: gray;
+        width: 50%;
+        border: 1px solid red;
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'JRow',
+    }
+</script>
+<style scoped lang="scss">
+    .row{
+        display: flex;
+        flex-wrap: wrap;
+    }
+</style>
